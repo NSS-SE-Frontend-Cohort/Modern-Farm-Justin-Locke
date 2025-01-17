342d50c0cb84e8d0deffae3ddee197ddf65870d0
Justin Locke <Justindeanlocke@gmail.com>
2025-01-17 13:47:38 -0600

    Added field module to add seeds to field and return field array

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,17 @@ console.log("Welcome to the main module")
 
 import { createPlan } from './plan.js';
 import { createCorn } from './seeds/corn.js';
+import { addPlant } from './field.js';
+import { usePlants } from './field.js';
 
 const yearlyPlan = createPlan();
 console.log(yearlyPlan);
 
-const corn = createCorn();
-console.log(corn);
+const cornSeed = createCorn();
+console.log(cornSeed);
+
+addPlant(cornSeed);
+addPlant(cornSeed);
+const field = usePlants();
+
+console.log('field = ',JSON.stringify(field));
