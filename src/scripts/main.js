6c6f931d3f0b3d9c04ea970f20223995dad9c400
Justin Locke <Justindeanlocke@gmail.com>
2025-01-17 13:21:25 -0600

    added seeds file and code for corn.js and asparagus.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,10 @@
 console.log("Welcome to the main module")
 
 import { createPlan } from './plan.js';
+import { createCorn } from './seeds/corn.js';
 
 const yearlyPlan = createPlan();
 console.log(yearlyPlan);
+
+const corn = createCorn();
+console.log(corn);
